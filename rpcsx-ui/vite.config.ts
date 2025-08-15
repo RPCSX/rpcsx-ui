db70519fc509080970d91b05bb41214fc179ee30
DH <dh.rpcs3@gmail.com>
2025-08-15 14:58:29 +0300

    update dependencies

--- rpcsx-ui/vite.config.ts
+++ rpcsx-ui/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, Plugin } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { rpcsx } from "rpcsx-ui-kit/build/main";
 
 export default defineConfig(async () => ({
     plugins: [
+        tailwindcss(),
         rpcsx() as Promise<Plugin[]>,
     ],
     build: {
         target: "esnext",
-        minify: false
+        minify: false,
     }
 }));
-
