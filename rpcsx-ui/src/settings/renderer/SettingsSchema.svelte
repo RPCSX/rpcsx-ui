5003fdd84e15ca68365bfa80b26e7ebef89eaf7b
DH <dh.rpcs3@gmail.com>
2025-08-16 10:52:37 +0300

    settings: fix undefined properties read

--- rpcsx-ui/src/settings/renderer/SettingsSchema.svelte
+++ rpcsx-ui/src/settings/renderer/SettingsSchema.svelte
@@ -3,7 +3,7 @@
     import type { Schema } from "$core/Schema";
     import Toggle from "./Toggle.svelte";
     import * as core from "$core";
-    import SettingsSchema from "./SettingsSchema.svelte"
+    import SettingsSchema from "./SettingsSchema.svelte";
 
     export let schema: Schema;
     export let value: any;
@@ -11,103 +11,107 @@
     export let active = 0;
 </script>
 
-{#if schema.type === "object"}
-    {#if depth === 0}
-        <div class="bg-neutral-900 flex h-full p-2 space-x-2">
-            <ul class="flex-col space-y-2">
-                {#each Object.keys(schema.properties) as property, index}
-                    <li>
-                        <button
-                            on:click={() => (active = index)}
-                            class="{active == index
-                                ? 'bg-blue-700/40'
-                                : 'hover:bg-neutral-700/40'} inline-flex items-center p-2 pe-10 w-full rounded active:bg-neutral-800/40 shadow-sm"
-                        >
-                            {#if "icon" in schema.properties[property]}
-                                <div class="w-5 h-5 me-2 text-white">
-                                    <Icon
-                                        src={schema.properties[property]?.icon}
-                                        solid
-                                    />
-                                </div>
-                            {/if}
+{#if schema !== undefined}
+    {#if schema.type === "object"}
+        {#if depth === 0}
+            <div class="bg-neutral-900 flex h-full p-2 space-x-2">
+                <ul class="flex-col space-y-2">
+                    {#each Object.keys(schema.properties) as property, index}
+                        <li>
+                            <button
+                                on:click={() => (active = index)}
+                                class="{active == index
+                                    ? 'bg-blue-700/40'
+                                    : 'hover:bg-neutral-700/40'} inline-flex items-center p-2 pe-10 w-full rounded active:bg-neutral-800/40 shadow-sm"
+                            >
+                                {#if "icon" in schema.properties[property]}
+                                    <div class="w-5 h-5 me-2 text-white">
+                                        <Icon
+                                            src={schema.properties[property]
+                                                ?.icon}
+                                            solid
+                                        />
+                                    </div>
+                                {/if}
 
-                            {schema.properties[property].label ?? property}
-                        </button>
-                    </li>
-                {/each}
-                <li>
+                                {schema.properties[property].label ?? property}
+                            </button>
+                        </li>
+                    {/each}
+                    <li>
                         <button
                             on:click={() => {
                                 active = Object.keys(schema.properties).length;
                                 core.popView();
                             }}
-                            class="{active == Object.keys(schema.properties).length
+                            class="{active ==
+                            Object.keys(schema.properties).length
                                 ? 'bg-blue-700/40'
                                 : 'hover:bg-neutral-700/40'} inline-flex items-center p-2 pe-10 w-full rounded active:bg-neutral-800/40 shadow-sm"
                         >
-
                             {"Exit"}
                         </button>
                     </li>
-            </ul>
-            <div
-                class="border border-neutral-600 bg-neutral-800 rounded p-2 h-full w-full"
-            >
-                <SettingsSchema
-                    schema={schema.properties[
-                        Object.keys(schema.properties)[active]
-                    ]}
-                    value={value[Object.keys(schema.properties)[active]]}
-                    depth={depth + 1}
-                ></SettingsSchema>
-                <!-- <svelte:component this={activeTab.content} /> -->
+                </ul>
+                <div
+                    class="border border-neutral-600 bg-neutral-800 rounded p-2 h-full w-full"
+                >
+                    <SettingsSchema
+                        schema={schema.properties[
+                            Object.keys(schema.properties)[active]
+                        ]}
+                        value={value[Object.keys(schema.properties)[active]]}
+                        depth={depth + 1}
+                    ></SettingsSchema>
+                    <!-- <svelte:component this={activeTab.content} /> -->
+                </div>
             </div>
-        </div>
-    {:else}
-        <div class="bg-neutral-900 flex h-full p-2 space-x-2">
-            <ul class="flex-col space-y-2">
-                {#each Object.keys(schema.properties) as property, index}
-                    <li>
-                        <button
-                            on:click={() => (active = index)}
-                            class="{active == index
-                                ? 'bg-blue-700/40'
-                                : 'hover:bg-neutral-700/40'} inline-flex items-center p-2 pe-10 w-full rounded active:bg-neutral-800/40 shadow-sm"
-                        >
-                            {#if "icon" in schema.properties[property]}
-                                <div class="w-5 h-5 me-2 text-white">
-                                    <Icon
-                                        src={schema.properties[property]?.icon}
-                                        solid
-                                    />
-                                </div>
-                            {/if}
+        {:else}
+            <div class="bg-neutral-900 flex h-full p-2 space-x-2">
+                <ul class="flex-col space-y-2">
+                    {#each Object.keys(schema.properties) as property, index}
+                        <li>
+                            <button
+                                on:click={() => (active = index)}
+                                class="{active == index
+                                    ? 'bg-blue-700/40'
+                                    : 'hover:bg-neutral-700/40'} inline-flex items-center p-2 pe-10 w-full rounded active:bg-neutral-800/40 shadow-sm"
+                            >
+                                {#if "icon" in schema.properties[property]}
+                                    <div class="w-5 h-5 me-2 text-white">
+                                        <Icon
+                                            src={schema.properties[property]
+                                                ?.icon}
+                                            solid
+                                        />
+                                    </div>
+                                {/if}
 
-                            {schema.properties[property].label ?? property}
-                        </button>
-                    </li>
-                {/each}
-            </ul>
-            <div
-                class="border border-neutral-600 bg-neutral-800 rounded p-2 h-full w-full"
-            >
-                <SettingsSchema
-                    schema={schema.properties[
-                        Object.keys(schema.properties)[active]
-                    ]}
-                    value={value[Object.keys(schema.properties)[active]]}
-                    depth={depth + 1}
-                ></SettingsSchema>
+                                {schema.properties[property].label ?? property}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+                <div
+                    class="border border-neutral-600 bg-neutral-800 rounded p-2 h-full w-full"
+                >
+                    <SettingsSchema
+                        schema={schema.properties[
+                            Object.keys(schema.properties)[active]
+                        ]}
+                        value={value[Object.keys(schema.properties)[active]]}
+                        depth={depth + 1}
+                    ></SettingsSchema>
+                </div>
             </div>
-        </div>
+        {/if}
+    {:else if schema.type === "boolean"}
+        <Toggle
+            value={value || schema.defaultValue}
+            label={schema.label ?? ""}
+            onchange={(newValue) => (value = newValue)}
+        />
+    {:else}
+        <h1>Unimplemented {schema.type}</h1>
     {/if}
-{:else if schema.type === "boolean"}
-    <Toggle
-        value={value || schema.defaultValue}
-        label={schema.label ?? ""}
-        onchange={(newValue) => (value = newValue)}
-    />
-{:else}
-    <h1> Unimplemented {schema.type} </h1>
 {/if}
